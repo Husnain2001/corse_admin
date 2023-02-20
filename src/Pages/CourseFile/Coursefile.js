55608059cb2ea4c329628b3f8d5be650994388a3
Aqib Jawad <62548286+aqibjawad@users.noreply.github.com>
2023-02-20 22:05:20 +0500

    admin push

--- src/Pages/CourseFile/Coursefile.js
+++ src/Pages/CourseFile/Coursefile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Coursefile.css";
 
 import {
@@ -9,109 +9,131 @@ import {
     Col,
 } from "react-bootstrap";
 
+import Finalexam from './finalexam';
+import Midexam from './Midexam';
+
+
 const Coursefile = () => {
+
+    const [page, setPage] = useState(1);
 
     return (
         <div className='Coursefile'>
             <Card className="rounded-0 border-0 mt-3">
-                <Card.Body className="pb-3 pt-0 pt-5">
-                    <h3 className="mb-3"> Add Courses </h3>
-                    <Row className="bg-white rounded-0 border-0">
+                {page === 1 ? (
+                    <Card.Body className="pb-3 pt-0 pt-5">
+                        <h3 className="mb-3"> Add Courses </h3>
+                        <Row className="bg-white rounded-0 border-0">
 
-                        <Col md={6}>
-                            <Form.Group className="">
-                                <Form.Label> Department </Form.Label>
-                                <Form.Control className="form-control" as="select">
-                                    <option>Choose Department</option>
-                                    <option value="1">BS Computer Sciences</option>
-                                    <option value="2">BS Industrial & Manufacturing Engineering</option>
-                                    <option value="3">BS Software Engineering</option>
-                                    <option value="4"> BS Electrical Engineering</option>
-                                    <option value="5">BS Physics</option>
-                                    <option value="6">BS MAth</option>
-                                </Form.Control>
-                            </Form.Group>
-                        </Col>
+                            <Col md={6}>
+                                <Form.Group className="">
+                                    <Form.Label> Department </Form.Label>
+                                    <Form.Control className="form-control" as="select">
+                                        <option>Choose Department</option>
+                                        <option value="1">BS Computer Sciences</option>
+                                        <option value="2">BS Industrial & Manufacturing Engineering</option>
+                                        <option value="3">BS Software Engineering</option>
+                                        <option value="4"> BS Electrical Engineering</option>
+                                        <option value="5">BS Physics</option>
+                                        <option value="6">BS MAth</option>
+                                    </Form.Control>
+                                </Form.Group>
+                            </Col>
 
-                        <Col md={6}>
-                            <Form.Group className="">
-                                <Form.Label> Session </Form.Label>
-                                <Form.Control className="form-control" as="select">
-                                    <option>Choose Department</option>
-                                    <option value="1"> 2019 </option>
-                                    <option value="2"> 2020 </option>
-                                    <option value="3"> 2021 </option>
-                                    <option value="4"> 2022 </option>
-                                </Form.Control>
-                            </Form.Group>
-                        </Col>
+                            <Col md={6}>
+                                <Form.Group className="">
+                                    <Form.Label> Session </Form.Label>
+                                    <Form.Control className="form-control" as="select">
+                                        <option>Choose Department</option>
+                                        <option value="1"> 2019 </option>
+                                        <option value="2"> 2020 </option>
+                                        <option value="3"> 2021 </option>
+                                        <option value="4"> 2022 </option>
+                                    </Form.Control>
+                                </Form.Group>
+                            </Col>
 
-                        <Col md={6}>
-                            <Form.Group className="mt-3">
-                                <Form.Label> Courses </Form.Label>
-                                <Form.Control className="form-control" as="select">
-                                    <option>Choose Department</option>
-                                    <option value="1">BS Computer Sciences</option>
-                                    <option value="2">BS Industrial & Manufacturing Engineering</option>
-                                    <option value="3">BS Software Engineering</option>
-                                    <option value="4"> BS Electrical Engineering</option>
-                                    <option value="5">BS Physics</option>
-                                    <option value="6">BS MAth</option>
-                                </Form.Control>
-                            </Form.Group>
-                        </Col>
+                            <Col md={6}>
+                                <Form.Group className="mt-3">
+                                    <Form.Label> Courses </Form.Label>
+                                    <Form.Control className="form-control" as="select">
+                                        <option>Choose Department</option>
+                                        <option value="1">BS Computer Sciences</option>
+                                        <option value="2">BS Industrial & Manufacturing Engineering</option>
+                                        <option value="3">BS Software Engineering</option>
+                                        <option value="4"> BS Electrical Engineering</option>
+                                        <option value="5">BS Physics</option>
+                                        <option value="6">BS MAth</option>
+                                    </Form.Control>
+                                </Form.Group>
+                            </Col>
 
-                        <Col md={6}>
-                            <Form.Group className="mt-3">
-                                <Form.Label> Teachers </Form.Label>
-                                <Form.Control className="form-control" as="select">
-                                    <option>Choose Department</option>
-                                    <option value="1">BS Computer Sciences</option>
-                                    <option value="2">BS Industrial & Manufacturing Engineering</option>
-                                    <option value="3">BS Software Engineering</option>
-                                    <option value="4"> BS Electrical Engineering</option>
-                                    <option value="5">BS Physics</option>
-                                    <option value="6">BS Mathematics</option>
-                                </Form.Control>
-                            </Form.Group>
-                        </Col>
+                            <Col md={6}>
+                                <Form.Group className="mt-3">
+                                    <Form.Label> Teachers </Form.Label>
+                                    <Form.Control className="form-control" as="select">
+                                        <option>Choose Department</option>
+                                        <option value="1">BS Computer Sciences</option>
+                                        <option value="2">BS Industrial & Manufacturing Engineering</option>
+                                        <option value="3">BS Software Engineering</option>
+                                        <option value="4"> BS Electrical Engineering</option>
+                                        <option value="5">BS Physics</option>
+                                        <option value="6">BS Mathematics</option>
+                                    </Form.Control>
+                                </Form.Group>
+                            </Col>
 
-                        <Col md={6}>
-                            <Form.Group className="mt-3">
-                                <Form.Label> Semester </Form.Label>
-                                <Form.Control className="form-control" as="select">
-                                    <option>Choose Department</option>
-                                    <option value="1">BS Computer Sciences</option>
-                                    <option value="2">BS Industrial & Manufacturing Engineering</option>
-                                    <option value="3">BS Software Engineering</option>
-                                    <option value="4"> BS Electrical Engineering</option>
-                                    <option value="5">BS Physics</option>
-                                    <option value="6">BS Mathematics</option>
-                                </Form.Control>
-                            </Form.Group>
-                        </Col>
+                            <Col md={6}>
+                                <Form.Group className="mt-3">
+                                    <Form.Label> Semester </Form.Label>
+                                    <Form.Control className="form-control" as="select">
+                                        <option>Choose Department</option>
+                                        <option value="1">BS Computer Sciences</option>
+                                        <option value="2">BS Industrial & Manufacturing Engineering</option>
+                                        <option value="3">BS Software Engineering</option>
+                                        <option value="4"> BS Electrical Engineering</option>
+                                        <option value="5">BS Physics</option>
+                                        <option value="6">BS Mathematics</option>
+                                    </Form.Control>
+                                </Form.Group>
+                            </Col>
 
-                        <Col md={6}>
-                            <Form.Group className="mt-3">
-                                <Form.Label> Course Outline </Form.Label>
-                                <InputGroup className="mb-3">
-                                    <input type="file" multiple accept="image/*" />
-                                </InputGroup>
-                            </Form.Group>
-                        </Col>
+                            <Col md={6}>
+                                <Form.Group className="mt-3">
+                                    <Form.Label> Course Outline </Form.Label>
+                                    <InputGroup className="mb-3">
+                                        <input type="file" multiple accept="image/*" />
+                                    </InputGroup>
+                                </Form.Group>
+                            </Col>
 
-                        <Col md={6}>
-                            <Form.Group className="">
-                                <Form.Label> Attendence </Form.Label>
-                                <InputGroup className="mb-3">
-                                    <input type="file" multiple accept="image/*" />
-                                </InputGroup>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <hr />
-                </Card.Body>
+                            <Col md={6}>
+                                <Form.Group className="">
+                                    <Form.Label> Attendence </Form.Label>
+                                    <InputGroup className="mb-3">
+                                        <input type="file" multiple accept="image/*" />
+                                    </InputGroup>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <hr />
+                    </Card.Body>
+                ) : ("")}
+
             </Card>
+            {page === 2 ? (
+                <Midexam />
+            ) : ("")}
+            {page === 3 ? (
+                <Finalexam />
+            ) : ("")}
+            {page === 1 ? (
+                <button onClick={() => setPage(1)}> Prev Page </button>
+            ) : ("")}
+
+            <button onClick={() => setPage(2)}> {page === 1 ? ("Next Page") : ("Prev Page")}</button>
+            {page === 1 ? ("") : (<button onClick={() => setPage(3)}> {page === 2 ? ("Next Page") : ("Next Page")} </button>)}
+
         </div>
     );
 
